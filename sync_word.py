90182aa24c7d1f1fe3196b614b472a1632a6eef6
Guilherme Theis <guilhermetheis15@gmail.com>
2018-08-13 10:23:21 +0200

    add sync word comparison and printing

--- sync_word.py
+++ sync_word.py
@@ -12,15 +12,20 @@ stdout = os.fdopen(sys.stdout.fileno(), 'wb', 0)
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument ('syncword', required=True)#Entry to the sync word
 parser.add_argument ('-ip', required =True) # IP Address
+parser.add_argument ('payload', type=int, required=True) #Payload size
 parser.add_argument ('-port', type=long, required =True) # Port number
 args = parser.parse_args() 
 
+sync_word_list = list(synword)
+bits_out = list()
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Creates the client socket
 
 client_socket.connect((args.ip, args.port)) #Connects the client to the server
 
+i = 0
 
 while True:
 	data = client_socket.recv(10000)
@@ -28,7 +33,17 @@ while True:
 		stdout.write('\n\nConnection Lost\n')
 		break
 	bits = bytearray(data)
-	bits_out = test = binascii.hexlify(bits)
-	stdout.write(bits_out)
-
+	bits_comp = binascii.b2a_hex(bits)
+	if (bits_comp == sync_word_list[i]) and (i < size(sync_word_list)): #comparing each element
+		bits_out.append(str(bits_comp))
+		i = i + 1
+	elif i < (size(sync_word_list) + payload): #allowing payload
+		bits_out.append(str(bits_comp))
+		i = i +1
+	else: #printing all
+		stdout.write("".join(bits_out))
+		bits_out = list() #deletign all the lit
 client_socket.close()
+
+##TODO -> Add hexadecimal conversion for input and output
+
