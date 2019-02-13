34af25f8d88a3c63822a7f42770ac408dc614251
Rafael <rmendesduarte@gmail.com>
2019-02-13 17:27:08 +0100

    organize docs

--- utils/generate_bin_data.py
+++ utils/generate_bin_data.py
@@ -1,10 +1,11 @@
-import struct
 from bitarray import bitarray
 from comm_utils import assemble_hdlc_packet
 
+# Description:
+# This script generates a binary file with sample packets to send to AX5043 radio. The packets are in HDLC frame format.
 
-preambule = b'\xAA'*600
-header = b'\x7E'
+PREAMBLE = b'\xAA'*16
+FLAG = b'\x7E'
 
 f = open('/tmp/tx_data.bin', 'wb')
 
@@ -12,20 +13,7 @@ for i in range(100):
     p1 = [x for x in range(31)]
     p1.append(i)
     payload_data = bytes(p1)
-    hdlc_packet = assemble_hdlc_packet(preambule, header, payload_data)
+    hdlc_packet = assemble_hdlc_packet(PREAMBLE, FLAG, payload_data)
     f.write(hdlc_packet)
-
-
-# preambule = '01111110'*8
-# data = '00110011'*32
-# postambule = '11101110'*2
-
-# simple_packet_bin = preambule + data + postambule
-# simple_packet = bitarray(simple_packet_bin)
-# # open file to write bytes
-# f.write(simple_packet.tobytes())
-
-# for n in range(0, int(len(varicode_string)/8)):
-# 	f.write(struct.pack('@B', u8))
 
 f.close()
